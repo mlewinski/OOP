757fac1863d8e3437fc7ee1dda685bface414582
Marek Lewiński <lewinski.marek@outlook.com>
2017-10-29 20:24:13 +0100

    Implemented StopWithTransfer class

--- Labs/lab02/src/com/pk/StopWithTransfer.java
+++ Labs/lab02/src/com/pk/StopWithTransfer.java
@@ -8,9 +8,18 @@ import java.util.Arrays;
 public class StopWithTransfer extends Stop{
     protected String[] Transfers;
 
-    StopWithTransfer(){
+    public StopWithTransfer(){
         super("-", null, 0, 0);
         Transfers = null;
+    }
+
+    public StopWithTransfer(String Name, String... transfers){
+        super(Name);
+        AssignTransfers(transfers);
+    }
+    public StopWithTransfer(String Name, String Next, int meters, int minutes, String... transfers){
+        super(Name, Next, meters, minutes);
+        AssignTransfers(transfers);
     }
 
     private void AssignTransfers(String... transfers){
@@ -18,5 +27,14 @@ public class StopWithTransfer extends Stop{
             Transfers = new String[transfers.length];
             Arrays.copyOf(transfers, transfers.length);
         }
+    }
+
+    @Override
+    public String toString(){
+        String s = super.toString();
+        String r = "Transfers : \n";
+        int i = 0;
+        for(String t : Transfers) r+=(i+" - "+t);
+        return s+r;
     }
 }
